e2ad270bf6031fe58f6c7f2e7f862d7ea5c71460
sparkofreason <dave.d.dixon@gmail.com>
2016-02-02 17:36:07 -0800

    Update midje

--- project.clj
+++ project.clj
@@ -1,18 +1,19 @@
 (defproject provisdom/math "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "(c) 2016 Provisdom Corporation"
+            :url  "http://www.provisdom.com"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [provisdom/utility-belt "0.1.0"]
                  [com.taoensso/truss "1.0.0"]
 
                  [provisdom/test "0.1.0" :scope "test"]
-                 [midje "1.6.3" :exclusions [org.clojure/clojure] :scope "test"]
+                 [midje "1.8.3" :exclusions [org.clojure/clojure] :scope "test"]
 
+                 [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.apache.commons/commons-math3 "3.6"]
                  [apache-commons-matrix "0.4.0"]
                  [clatrix "0.5.0"]
                  [net.mikera/core.matrix "0.49.0"]]
-  :plugins [[lein-midje "3.1.3"]])
+  :plugins [[lein-midje "3.2"]])
