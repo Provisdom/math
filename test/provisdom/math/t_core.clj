46ef5cee74232bb5321a22563288cadef231bef9
Kenny Williams <kennyjwilli@gmail.com>
2016-05-03 11:41:37 -0700

    Start writing midje converter. Get rid of test.check for trivial functions

--- test/provisdom/math/t_core.clj
+++ test/provisdom/math/t_core.clj
@@ -3,48 +3,17 @@
             [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
+            [clojure.test :refer [deftest is]]
+            [midje.sweet :as midj]
+            [midje.parsing.1-to-explicit-form.metadata :as mparse]
+            [midje.parsing.1-to-explicit-form.parse-background :as mbparse]
             [provisdom.math.core :as m]))
 
-(def iter-num 100)
+(defmacro convert-midje
+  [facts]
+  (let [[metadata forms] (mparse/separate-metadata facts)
+        separate-forms (partition 3 forms)]
+    ))
 
-(def simple-type
-  (gen/one-of [gen/int gen/large-integer gen/double gen/char
-               gen/string gen/ratio gen/boolean gen/keyword gen/symbol]))
-
-(defn- long-able*
-  [f v & tests]
-  (let [v? (f v)]
-    (= v?
-       (if (number? v)
-         (if (every? identity (apply list (m/roughly-round? v 0.0) (#'m/long-range? v) (map #(% v) tests)))
-           true
-           false)
-         false))))
-
-(defspec long-able?-check
-         iter-num
-         (prop/for-all [v simple-type] (long-able* m/long-able? v)))
-
-(defspec long-able+?-check
-         iter-num
-         (prop/for-all [v simple-type] (long-able* m/long-able+? v #(pos? %))))
-
-(defspec long-able-?-check
-         iter-num
-         (prop/for-all [v simple-type] (long-able* m/long-able-? v #(neg? %))))
-
-(defspec long-able-non+?-check
-         iter-num
-         (prop/for-all [v simple-type] (long-able* m/long-able-non+? v #(or (neg? %) (zero? %)))))
-
-(defspec long-able-non-?-check
-         iter-num
-         (prop/for-all [v simple-type] (long-able* m/long-able-non-? v #(or (pos? %) (zero? %)))))
-
-(defspec maybe-long-able-check
-         iter-num
-         (prop/for-all [v simple-type]
-                       (let [res (m/maybe-long-able v)]
-                         (if (m/long-able? v)
-                           (= (type res) Long)
-                           (= (type res) (type v))))))
+(deftest long-able?
+  (is ()))
