eb24ab488bc930986e1315302e0817c014ff4a0d
Mark Erickson <mark@provisdom.com>
2018-08-07 15:49:21 -0700

    added some neanderthal, and fixed lls returning extra rows

--- src/provisdom/math/neanderthal_matrix.clj
+++ src/provisdom/math/neanderthal_matrix.clj
@@ -17,6 +17,18 @@
   [rows columns v by-column?]
   (native/dge rows columns v {:layout (if by-column? :column :row)}))
 
+(defn ->neanderthal-matrix
+  ""
+  [nrows ncols]
+  (native/dge nrows ncols))
+
+(defn ->identity-neanderthal-matrix
+  ""
+  [size]
+  (let [zero (->neanderthal-matrix size size)
+        _ (neanderthal/entry! (neanderthal/dia zero) 1.0)]
+    zero))
+
 (defn matrix->neanderthal-matrix
   ""
   [m]
@@ -49,6 +61,11 @@
   [neanderthal-mx]
   (neanderthal/ncols neanderthal-mx))
 
+(defn transpose
+  ""
+  [neanderthal-mx]
+  (neanderthal/trans neanderthal-mx))
+
 (defn mx*
   "Matrix multiplication."
   [neanderthal-mx1 neanderthal-mx2]
@@ -60,21 +77,47 @@
   (fluokitten/fmap f a))
 
 (defn lls
-  "Linear Linear Squares, solving for 'x', where `a` × x = `b`."
+  "Linear Linear Squares, solving for 'x', where `a` × x = `b`.  Returns
+  solution."
   [a b]
-  (try (linear-algebra/ls a b)
+  (try (let [cols (columns a)
+             sol (linear-algebra/ls a b)
+             cols-sol (columns sol)]
+         (neanderthal/submatrix sol cols cols-sol))
        (catch Exception _ {::anomalies/category ::anomalies/no-solve
-                           ::anomalies/message "No LLS Solution"
-                           ::anomalies/fn (var lls)})))
+                           ::anomalies/message  "No LLS Solution"
+                           ::anomalies/fn       (var lls)})))
 
 (defn lls!
-  "Linear Linear Squares, solving for 'x', where `a` × x = `b`. After
-  destruction, `a` will contain factorization data, and `b` will contain matrix
-  solution. Also returns solution."
+  "Linear Linear Squares, solving for 'x', where `a` × x = `b`.  After
+  destruction, `a` will contain factorization data, and `b` will contain
+  solution.  Also returns solution."
   [a b]
-  (try (linear-algebra/ls! a b)
+  (try (let [cols (columns a)
+             sol (linear-algebra/ls! a b)
+             cols-sol (columns sol)]
+         (neanderthal/submatrix sol cols cols-sol))
        (catch Exception _ {::anomalies/category ::anomalies/no-solve
-                           ::anomalies/message "No LLS Solution"
-                           ::anomalies/fn (var lls!)})))
+                           ::anomalies/message  "No LLS Solution"
+                           ::anomalies/fn       (var lls!)})))
+
+(defn lls-with-projection-and-annihilation
+  "Linear Linear Squares, solving for 'x', where `a` × x = `b`.  Returns map
+  of solution, projection matrix, and annihilation matrix."
+  [a b]
+  (try (let [qr (linear-algebra/qrf a)
+             q (linear-algebra/org qr)
+             r1 (neanderthal/view-tr (:or qr) {:uplo :upper})
+             solution (linear-algebra/sv! r1 (mx* (transpose q) b))
+             projection (mx* q (transpose q))
+             cols (columns projection)
+             identity (->identity-neanderthal-matrix cols)
+             annihilation (neanderthal/axpy -1.0 projection identity)]
+         {:solution     solution
+          :projection   projection
+          :annihilation annihilation})
+       (catch Exception _ {::anomalies/category ::anomalies/no-solve
+                           ::anomalies/message  "No LLS Solution"
+                           ::anomalies/fn       (var lls-with-projection-and-annihilation)})))
 
 
